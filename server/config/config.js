389302aa46806a06c1eeb5e3bd8c11c11a979ffe
David Maman <dmaman86@gmail.com>
2018-09-29 04:43:42 +0300

    Variable MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlDB;
 if ( process.env.NODE_ENV === 'dev' ) {
     urlDB = 'mongodb://localhost:27017/coffee';
 } else {
-    urlDB = 'mongodb://coffe-user:davamc86@ds115533.mlab.com:15533/db_coffee';
+    urlDB = process.env.MONGO_URI;
 }
 
 
